81a04a096fd28b67c6b65975c478225b97d81756
Fynn <enriquefynn@gmail.com>
2015-02-17 21:48:34 -0200

    fixing get results

--- scripts/getResults.js
+++ scripts/getResults.js
@@ -14,15 +14,16 @@ mongoose.connection.on('error', function(err){
 co(function*(){
     try{
         var usersQ = User.find({});
-        var users = yield usersQ.exec(); 
+        var users = yield usersQ.exec();
         for (var i = 0; i < users.length; ++i){
             var bigStr = "";
+            console.log('Adding', users[i].wifiLog.length, 'records to user', users[i].username);
             for (var j = 0; j < users[i].wifiLog.length; ++j){
                 var timestamp = new Date(users[i].wifiLog[j].timestamp).getTime();
                 var name = users[i].wifiLog[j].name;
                 bigStr+= timestamp + ' ' + name + '\n';
             }
-            fs.appendFileSync(users[i].username + '-wifi.txt', 
+            fs.appendFileSync(users[i].username + '-wifi.txt',
                 bigStr);
             bigStr = "";
             for (var j = 0; j < users[i].locationLog.length; ++j){
@@ -32,16 +33,20 @@ co(function*(){
                 bigStr+= timestamp + ' ' + lat + ' ' + lon + '\n';
             }
 
-            fs.appendFileSync(users[i].username + '-gps.txt', 
+            fs.appendFileSync(users[i].username + '-gps.txt',
                 bigStr);
             users[i].locationLog = [];
             users[i].wifiLog = [];
-            users[i].save();
+            users[i].save(function(err, data){
+                ++cont;
+                if (err)
+                    console.error(err);
+                if (cont == users.length)
+                    mongoose.disconnect();
+            });
         }
-        process.exit();
     }
     catch(err){
         console.log(err);
     }
 });
-
